a73e68f2334eeb00f19eb07e32bb74cabb758755
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-09-12 15:35:49 -0300

    Upd: diseño appbar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,9 +5,44 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //argumento desde home
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-data';
     return const Scaffold(
-      body: Center(
-        child: Text('DetailsScreen'),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        title: Text("movie.title",
+            style: TextStyle(shadows: [
+              Shadow(
+                color: Colors.black,
+                blurRadius: 2.0,
+                offset: Offset(.5, .5),
+              )
+            ])),
+        background: FadeInImage(
+          fit: BoxFit.cover,
+          //Todo: replace for asset image
+          placeholder: NetworkImage("https://placehold.co/500x200/png"),
+          image: NetworkImage("https://placehold.co/500x200/png"),
+        ),
       ),
     );
   }
