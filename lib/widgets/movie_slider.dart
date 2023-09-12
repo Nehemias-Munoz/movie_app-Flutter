2ec619cc2638b7dfed31cc5b1cb20aff67baabbe
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-09-12 14:38:54 -0300

    Upd: diseño movie poster

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -8,26 +8,66 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: AppConstant.getSize(context).height * 0.25,
-      color: Colors.amber,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Padding(
-            padding: EdgeInsets.only(left: 20.0),
-            child: Text('Populares'),
+            padding: EdgeInsets.only(left: 12.0),
+            child: Text(
+              'Populares',
+              style: TextStyle(fontSize: 20),
+            ),
           ),
           Expanded(
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemBuilder: (_, __) => Container(
-                margin: const EdgeInsets.all(7.0),
-                color: Colors.red,
-                height: AppConstant.getSize(context).height * .9,
-                width: AppConstant.getSize(context).width * .35,
-              ),
+              itemBuilder: (_, __) => _MoviePoster(),
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class _MoviePoster extends StatelessWidget {
+  const _MoviePoster({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, 'details'),
+      child: Container(
+        margin: const EdgeInsets.all(7.0),
+        height: AppConstant.getSize(context).height * .9,
+        width: AppConstant.getSize(context).width * .30,
+        child: const Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+                child: FadeInImage(
+                  fit: BoxFit.contain,
+                  //Todo: replace with assetimage
+                  placeholder: NetworkImage('https://placehold.co/500x700/png'),
+                  image: NetworkImage('https://placehold.co/500x700/png'),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Text(
+                  'title',
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
